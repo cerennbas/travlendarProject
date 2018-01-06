018fc47e05a2c5b48ab3c173399fbcb71dd54eb8
Volga Araslı <v.arasli@yahoo.com>
2018-01-07 01:36:55 +0300

    Update SpringTravlendarApplication.java

--- src/main/java/com/travlendar/springtravlendar/SpringTravlendarApplication.java
+++ src/main/java/com/travlendar/springtravlendar/SpringTravlendarApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration (exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringTravlendarApplication {
 
 	public static void main(String[] args) {
