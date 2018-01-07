0b4f3a39474435742d2913bf91d4734ad3fb56ca
cerennbas <cerennbas@gmail.con>
2018-01-08 01:21:24 +0300

    meetingcontroller changed

--- src/main/java/com/travlendar/springtravlendar/controller/MeetingController.java
+++ src/main/java/com/travlendar/springtravlendar/controller/MeetingController.java
@@ -6,13 +6,19 @@ import com.travlendar.springtravlendar.model.User;
 import com.travlendar.springtravlendar.service.MeetingService;
 import com.travlendar.springtravlendar.service.UserService;
 import com.travlendar.springtravlendar.service.GoogleMapsService;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -26,8 +32,8 @@ public class MeetingController {
     private GoogleMapsService googleMapsService;
 
     @RequestMapping(value="/user/allMeeting")
-    public ResponseEntity<List<Meeting>> getMeetings(HttpSession httpSession) {
-        String email = (String) httpSession.getAttribute("email");
+    public ResponseEntity<List<Meeting>> getMeetings(HttpSession httpSession, @RequestBody Map<String, String> json) {
+        String email = json.get("email");
 
         if(email == null) {
             throw new TravlendarException("no session found", HttpStatus.BAD_REQUEST);
@@ -40,15 +46,31 @@ public class MeetingController {
     }
 
     @RequestMapping(value="/addMeeting", method = RequestMethod.POST)
-    public ResponseEntity<?> addMeeting(HttpSession httpSession, @RequestBody Meeting meeting) throws Exception {
-        String email = (String) httpSession.getAttribute("email");
+    public ResponseEntity<?> addMeeting(HttpSession httpSession, @RequestBody Map<String, String> json) throws Exception {
+        String email = json.get("email");
+        String description = json.get("description");
+        String startLocation = json.get("startLocation");
+        String location = json.get("location");
+        String endTime = json.get("endTime");
+        String startTime = json.get("startTime");
 
         if(email == null) {
             throw new TravlendarException("no session found", HttpStatus.BAD_REQUEST);
         }
+
         User user = userService.findUserByEmail(email);
 
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"); // your format
+        Date date = format.parse(startTime);
+
+        Meeting meeting = new Meeting();
+        meeting.setDescription(description);
+        meeting.setLocation(location);
+        meeting.setStartLocation(startLocation);
+        meeting.setStartTime(format.parse(startTime));
+        meeting.setEndTime(format.parse(endTime));
         meeting.setUser(user);
+
         String duration = googleMapsService.travelTime(meeting.getStartLocation(), meeting.getLocation());
         meeting.setDuration(duration);
 
