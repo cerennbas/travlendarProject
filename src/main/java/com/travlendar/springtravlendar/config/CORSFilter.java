59dcef7d5a906bb6aa3b3f265344617e42354a07
Volga Araslı <v.arasli@yahoo.com>
2018-01-07 04:42:29 +0300

    Update CORSFilter.java

--- src/main/java/com/travlendar/springtravlendar/config/CORSFilter.java
+++ src/main/java/com/travlendar/springtravlendar/config/CORSFilter.java
@@ -1,21 +1,14 @@
 package com.travlendar.springtravlendar.config;
+import java.io.IOException;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsFilter;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CORSFilter extends CorsFilter {
