43251ef438dafb86d3bcf60b275e30a3fc9cd2ef
Volga Araslı <v.arasli@yahoo.com>
2018-01-07 03:12:41 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/travlendar/springtravlendar/security/WebSecurityConfig.java
+++ src/main/java/com/travlendar/springtravlendar/security/WebSecurityConfig.java
@@ -10,7 +10,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/", "/login", "/signup").permitAll()
+                .antMatchers("/", "/api/login", "/api/signup").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
